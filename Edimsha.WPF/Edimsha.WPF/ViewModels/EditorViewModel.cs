6d80acd6d8281218aa2da4a36f6533e46818c6ff
Nestorm <nesdum18@yahoo.es>
2021-03-25 10:16:36 +0100

    UI actualizada

--- Edimsha.WPF/Edimsha.WPF/ViewModels/EditorViewModel.cs
+++ Edimsha.WPF/Edimsha.WPF/ViewModels/EditorViewModel.cs
@@ -4,6 +4,76 @@ namespace Edimsha.WPF.ViewModels
 {
     public class EditorViewModel : ViewModelBase
     {
+        // IOC
+
+        // Properties
+
+        #region Checkboxes
+
+        private bool _cleanListOnExit;
+        private bool _addOnReplace;
+        private bool _keepOriginalResolution;
+        private bool _optimizeImage;
+        private bool _replaceForOriginal;
+
+        public bool CleanListOnExit
+        {
+            get => _cleanListOnExit;
+            set
+            {
+                _cleanListOnExit = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool AddOnReplace
+        {
+            get => _addOnReplace;
+            set
+            {
+                _addOnReplace = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool KeepOriginalResolution
+        {
+            get => _keepOriginalResolution;
+
+            set
+            {
+                _keepOriginalResolution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool OptimizeImage
+        {
+            get => _optimizeImage;
+
+            set
+            {
+                _optimizeImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool ReplaceForOriginal
+        {
+            get => _replaceForOriginal;
+
+            set
+            {
+                _replaceForOriginal = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        // Commands
+
+        // Constructor
         public EditorViewModel()
         {
             Console.WriteLine("Test EDITOR-VM");
