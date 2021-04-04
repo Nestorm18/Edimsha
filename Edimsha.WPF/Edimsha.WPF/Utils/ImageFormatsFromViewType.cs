537da2bc53756db922571f7318b947cf7c8ce6b9
Nestorm <nesdum18@yahoo.es>
2021-04-04 12:46:42 +0100

    Documentacion añadida

--- Edimsha.WPF/Edimsha.WPF/Utils/ImageFormatsFromViewType.cs
+++ Edimsha.WPF/Edimsha.WPF/Utils/ImageFormatsFromViewType.cs
@@ -7,8 +7,14 @@ namespace Edimsha.WPF.Utils
 {
     public static class ImageFormatsFromViewType
     {
+        /// <summary>
+        /// Gets a list of the available formats for the mode that is requested by parameter.
+        /// </summary>
+        /// <param name="parameter">A type of <see cref="ModeImageTypes"/></param>
+        /// <returns>List of avaliable formats for requested <see cref="ModeImageTypes"/>.</returns>
         public static IEnumerable GetImageType(object parameter)
         {
+            // Gets all the values of the Enum and returns.
             IEnumerable imageTypes = parameter switch
             {
                 ModeImageTypes.Editor => Enum.GetValues(typeof(ImageTypesEditor)).Cast<ImageTypesEditor>(),
