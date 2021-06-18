06c79c1ba5cbb891e0ce488d54d88c22679cc85b
Nestorm <nesdum18@yahoo.es>
2021-06-18 10:26:51 +0200

    Añadir CLI

---
+++ Edimsha.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Edimsha.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // https://github.com/Nestorm18/TwitchDownloader/blob/master/TwitchDownloaderCLI/Options.cs
+            // https://github.com/Nestorm18/TwitchDownloader/blob/9708088fb6d687923b62c6e6220c22425b81ec3a/TwitchDownloaderCLI/Program.cs#L71
+        }
+    }
+}
