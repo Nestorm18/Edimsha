7939bc70c2d51863e4833076ed66d8a925fbc785
Nestorm <nesdum18@yahoo.es>
2021-06-19 12:28:25 +0200

    Filtro movido

--- Edimsha.WPF/Edimsha.WPF/Commands/OpenImagesCommand.cs
+++ Edimsha.WPF/Edimsha.WPF/Commands/OpenImagesCommand.cs
@@ -37,7 +37,7 @@ namespace Edimsha.WPF.Commands
         /// <param name="parameter">A current <see cref="ViewType"/> in use.</param>
         public async void Execute(object? parameter)
         {
-            var filter = CreateFilter(ImageFormatsFromViewType.GetImageType(parameter));
+            var filter = FilterCreator.Create(ImageFormatsFromViewType.GetImageType(parameter));
 
             var urls = await _dialogService.OpenFileSelector(
                 TranslationSource.GetTranslationFromString("select_images"), filter, true);
@@ -50,37 +50,9 @@ namespace Edimsha.WPF.Commands
                 urls.ToArray(),
                 ModeImageTypes.Editor);
 
-           _viewModel.PathList.Clear();
+            _viewModel.PathList.Clear();
 
             foreach (var s in listCleaned) _viewModel.PathList.Add(s);
-        }
-
-        /// <summary>
-        /// Creates the filter used in file browsers based on the formats supported by the application at the current time.
-        /// </summary>
-        /// <param name="getImageType">All avaliable format for current <see cref="ViewType"/>.</param>
-        /// <returns>Formmated string to use as filter.</returns>
-        private static string CreateFilter(IEnumerable getImageType)
-        {
-            var builder = new StringBuilder();
-            var builderSecond = new StringBuilder();
-            builder.Append(TranslationSource.GetTranslationFromString("image_files"));
-            builder.Append(' ');
-            builder.Append('(');
-
-            foreach (var type in getImageType)
-            {
-                var format = $"*.{type.ToString()?.ToLower()};";
-                builder.Append(format);
-                builderSecond.Append(format);
-            }
-
-            builder.Remove(builder.Length - 1, 1);
-            builder.Append(')');
-            builder.Append('|');
-            builder.Append(builderSecond);
-
-            return builder.ToString();
         }
 
         public event EventHandler? CanExecuteChanged;
---
+++ Edimsha.WPF/Edimsha.WPF/Utils/FilterCreator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Text;
+using Edimsha.Core.Language;
+using Edimsha.WPF.State.Navigators;
+
+namespace Edimsha.WPF.Utils
+{
+    public static class FilterCreator
+    {
+        /// <summary>
+        /// Creates the filter used in file browsers based on the formats supported by the application at the current time.
+        /// </summary>
+        /// <param name="imageType">All avaliable format for current <see cref="ViewType"/>.</param>
+        /// <returns>Formmated string to use as filter.</returns>
+        public static string Create(IEnumerable imageType)
+        {
+            var builder = new StringBuilder();
+            var builderSecond = new StringBuilder();
+            builder.Append(TranslationSource.GetTranslationFromString("image_files"));
+            builder.Append(' ');
+            builder.Append('(');
+
+            foreach (var type in imageType)
+            {
+                var format = $"*.{type.ToString()?.ToLower()};";
+                builder.Append(format);
+                builderSecond.Append(format);
+            }
+
+            builder.Remove(builder.Length - 1, 1);
+            builder.Append(')');
+            builder.Append('|');
+            builder.Append(builderSecond);
+
+            return builder.ToString();
+        }
+    }
+}
