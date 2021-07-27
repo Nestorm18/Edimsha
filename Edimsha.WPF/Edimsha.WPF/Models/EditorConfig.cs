936bedbdf8d89da6a4c5e551d11feff3af81b6bf
Nestorm <nesdum18@yahoo.es>
2021-07-27 16:42:26 +0200

    Sin usar de forma temporal

---
+++ Edimsha.WPF/Edimsha.WPF/Models/EditorConfig.cs
@@ -0,0 +1,19 @@
+﻿using Edimsha.Core.Models;
+
+namespace Edimsha.WPF.Models
+{
+    public class EditorConfig
+    {
+        public string Language { get; set; }
+        public bool CleanListOnExit { get; set; }
+        public bool IterateSubdirectories { get; set; }
+        public string OutputFolder { get; set; }
+        public string Edimsha { get; set; }
+        public bool AlwaysIncludeOnReplace { get; set; }
+        public bool KeepOriginalResolution { get; set; }
+        public double CompresionValue { get; set; }
+        public bool OptimizeImage { get; set; }
+        public bool ReplaceForOriginal { get; set; }
+        public Resolution Resolution { get; set; }
+    }
+}
