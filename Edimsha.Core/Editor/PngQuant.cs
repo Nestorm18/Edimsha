aa24e5649c1fb23bc7cc3beb7bf371a62502fafa
Nestorm <nesdum18@yahoo.es>
2021-08-21 11:37:32 +0200

    Fix value over 255 bits

--- Edimsha.Core/Editor/PngQuant.cs
+++ Edimsha.Core/Editor/PngQuant.cs
@@ -10,9 +10,11 @@ namespace Edimsha.Core.Editor
 
         public static void ExecutePngQuant(string path, double bitNumber)
         {
+            if (bitNumber > 255) bitNumber = 255;
+
             var psi = new ProcessStartInfo(Pngquant)
             {
-                Arguments = $"-f --ext {Path.GetExtension(path)} --skip-if-larger --quality {(int)bitNumber} \"{path}\"",
+                Arguments = $"-f --ext {Path.GetExtension(path)} --skip-if-larger --quality {(int) bitNumber} \"{path}\"",
                 UseShellExecute = false,
                 CreateNoWindow = true
             };
