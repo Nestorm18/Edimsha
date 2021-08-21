3cfd9846f590d4910483c797db49403b8e6b5805
Nestorm <nesdum18@yahoo.es>
2021-08-21 11:12:28 +0200

    Documentacion BaseEditor.cs

--- Edimsha.Core/BaseImageEdition/BaseEditor.cs
+++ Edimsha.Core/BaseImageEdition/BaseEditor.cs
@@ -4,6 +4,13 @@ namespace Edimsha.Core.BaseImageEdition
 {
     public class BaseEditor
     {
+        /// <summary>
+        /// Generates a save path for the current image passed as a path using the values provided.
+        /// </summary>
+        /// <param name="outputFolder">A image saving folder.</param>
+        /// <param name="path">A current image path.</param>
+        /// <param name="edimsha">A value of edimsha.</param>
+        /// <returns>The new route generated using the parameters.</returns>
         protected static string GeneratesavePath(string outputFolder, string path, string edimsha)
         {
             var name = GenerateName(outputFolder, path, edimsha);
@@ -13,6 +20,13 @@ namespace Edimsha.Core.BaseImageEdition
                 : Path.Combine(outputFolder, name);
         }
 
+        /// <summary>
+        /// Generates the name of the image using the values passed as parameters.
+        /// </summary>
+        /// <param name="outputFolder">A image saving folder.</param>
+        /// <param name="path">A current image path.</param>
+        /// <param name="edimsha">A value of edimsha.</param>
+        /// <returns>The generated name.</returns>
         private static string GenerateName(string outputFolder, string path, string edimsha)
         {
             var samePath = IsSamePath(outputFolder, path);
@@ -23,6 +37,12 @@ namespace Edimsha.Core.BaseImageEdition
             return $"{edimsha}{imageName}";
         }
 
+        /// <summary>
+        /// Check that the path to save the image is different from the current path where the image is located.
+        /// </summary>
+        /// <param name="outputFolder">A image saving folder.</param>
+        /// <param name="path">A current image path.</param>
+        /// <returns>True if is same path.</returns>
         private static bool IsSamePath(string outputFolder, string path)
         {
             var outputDir = outputFolder;
